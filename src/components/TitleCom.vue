c683c3a5ae7a7fd2916442a109679040b3074e04
吴章政 <wuzhangzheng@hotmail.com>
2022-02-20 08:59:11 +0800

    add titlecom

---
+++ src/components/TitleCom.vue
@@ -0,0 +1,27 @@
+<template lang="">
+    <div class="title-box">
+        <div class="left">
+            <div class="left">
+                <img src="../assets/logo.png" alt="">
+                <h4>招聘网站</h4>
+            </div>
+            
+        </div>
+        <div class="right">
+            发布
+        </div>
+    </div>
+</template>
+<script lang="ts">
+export default {
+    
+}
+</script>
+<style lang="scss">
+.title-box{
+    height: 100px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+</style>
---
+++ src/views/IndexView.vue
@@ -0,0 +1,21 @@
+<template lang="">
+    <div>
+        列表页
+        <TitleCom/>
+        
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TitleCom from '@/components/TitleCom.vue'
+
+export default defineComponent({
+  name: 'IndexView',
+  components: {
+    TitleCom,
+  },
+});
+</script>
+<style lang="">
+    
+</style>
